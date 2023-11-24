6ba7a6af15bfbb01efc73a2264d76370090df40e
Egor K <kirdyashkin@aeroidea.ru>
2023-11-24 16:42:35 +0300

    bug: fixed dynamic icon

--- admin-ui/src/ui/organisms/DynamicIcon/DynamicIcon.tsx
+++ admin-ui/src/ui/organisms/DynamicIcon/DynamicIcon.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import loadable from '@loadable/component';
-import { IconLoader } from '@tabler/icons-react';
+import { IconCircle, IconLoader } from '@tabler/icons-react';
 
 const AsyncIcon = loadable(() => import(`@tabler/icons-react`), {
   resolveComponent: (components: any, props: Record<string, string>) => components[props.name],
@@ -12,7 +12,11 @@ interface DynamicIconProps {
 }
 
 const DynamicIcon: FC<DynamicIconProps> = ({ name }) => {
-  return <AsyncIcon name={name} />;
+  if (name.length > 0 && name !== '') {
+    return <AsyncIcon name={name} />;
+  } else {
+    return <IconCircle />;
+  }
 };
 
 export default DynamicIcon;
